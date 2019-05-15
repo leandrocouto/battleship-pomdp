b171779ea9110f6b7c0b4c70b38114d96d6e7146
Leandro Couto Medeiros <leandrocoutom@gmail.com>
2019-05-15 16:40:40 -0300

    ADD changes in the initial modelling of the map

--- main.py
+++ main.py
@@ -18,84 +18,14 @@ class Ship:
         self.length = length
     def print_ship(self):
         print('x: %d, y: %d, direction: %d, length: %d' %(self.x,self.y,self.direction,self.length))
-    def is_valid(self, battlefield):
-        if self.direction == 0:
-            print('Sou up. x: ', self.x, ' y: ', self.y, ' length: ', self.length)
-        #There's already a ship in this exact coordinate
-        if battlefield.grid[self.x][self.y] == 1:
-            print('Deu errado')
-            return False
-        else:
-            #Check if it's possible to place this ship according to its direction and length
-            #up
-            if self.direction == 0:
-                if self.length > (self.x+1):
-                    return False
-                else:
-				#Check for ships in the way
-                    for i in range(self.x-1, self.x-self.length, -1):
-                        if battlefield.grid[i][self.y] == 1:
-                            return False
-				#Check for ships in the neighborhood
-                    for i in range(self.x, self.x-self.length, -1):
-                        if i == self.x:
-                            if (battlefield.check_cell_for_ship(i+1, self.y) or battlefield.check_cell_for_ship(i+1, self.y-1)
-								or battlefield.check_cell_for_ship(i+1, self.y+1) or battlefield.check_cell_for_ship(i, self.y-1)
-								or battlefield.check_cell_for_ship(i, self.y+1)) == True:
-                                    return False
-                        else:
-                            if (battlefield.check_cell_for_ship(i-1, self.y-1)
-								or battlefield.check_cell_for_ship(i-1, self.y+1) or battlefield.check_cell_for_ship(i, self.y-1)
-								or battlefield.check_cell_for_ship(i, self.y+1)) == True:
-                                    return False
-                            
-            #right
-            elif self.direction == 1: 
-                if self.length > (battlefield.columns - self.y):
-                    return False
-                else:
-					#Check for ships in the way
-                    for i in range(self.y+1, self.y+self.length):
-                        if battlefield.grid[self.x][i] == 1:
-                            return False
-                    #Check for ships in the neighborhood
-                    for i in range(self.y, self.y+self.length):
-                        if i == self.y:
-                            if (battlefield.check_cell_for_ship(i+1, self.y) or battlefield.check_cell_for_ship(i+1, self.y-1)
-								or battlefield.check_cell_for_ship(i+1, self.y+1) or battlefield.check_cell_for_ship(i, self.y-1)
-								or battlefield.check_cell_for_ship(i, self.y+1)) == True:
-                                    return False
-                        else:
-                            if (battlefield.check_cell_for_ship(i-1, self.y-1)
-								or battlefield.check_cell_for_ship(i-1, self.y+1) or battlefield.check_cell_for_ship(i, self.y-1)
-								or battlefield.check_cell_for_ship(i, self.y+1)) == True:
-                                    return False
-            #down
-            elif self.direction == 2: 
-                if self.length > (battlefield.rows - self.x):
-                    return False
-                else:
-					#Check for ships in the way
-                    for i in range(self.x+1, self.x+self.length):
-                        if battlefield.grid[self.x][i] == 1:
-                            return False
-            #left
-            elif self.direction == 3: 
-                if self.length > (self.y+1):
-                    return False
-                else:
-					#Check for ships in the way
-                    for i in range(self.y-1, self.y-self.length, -1):
-                        if battlefield.grid[i][self.y] == 1:
-                            return False
-            return True        
 
 class Grid:
     def __init__(self, rows, columns):
         self.rows = rows
         self.columns = columns
         self.grid = np.zeros((rows, columns))
-
+    
+    #Assume ship is already valid
     def place_ship(self,ship):
         for i in range(ship.length):
             if ship.direction == 0: #up
@@ -106,13 +36,69 @@ class Grid:
                 self.grid[ship.x + i][ship.y] = 1
             elif ship.direction == 3: #left
                 self.grid[ship.x][ship.y - i] = 1
-    def check_cell_for_ship(self, x, y):
+    def check_for_ship(self, x, y):
         if x >= self.rows or x < 0 or y >= self.columns or y < 0:
             return False
         elif self.grid[x][y] == 1:
             return True
         else:
             return False
+    def is_ship_valid(self, ship):
+        #up
+        if ship.direction == 0:
+            #if the ship won't fit
+            if ship.length > (ship.x + 1):
+                return False
+            else:
+                #Check for ships in the neighborhood
+                for i in range(ship.x, ship.x-ship.length, -1):
+                    if (self.check_for_ship(i+1, ship.y-1) or self.check_for_ship(i+1, ship.y)
+								or self.check_for_ship(i+1, ship.y+1) or self.check_for_ship(i, ship.y-1)
+								or self.check_for_ship(i, ship.y) or self.check_for_ship(i, ship.y+1)
+                                or self.check_for_ship(i-1, ship.y-1) or self.check_for_ship(i-1, ship.y)
+                                or self.check_for_ship(i-1, ship.y+1)):
+                                    return False
+        #right
+        elif ship.direction == 1:
+            #if the ship won't fit
+            if ship.length > (self.columns - ship.y):
+                    return False
+            else:
+                #Check for ships in the neighborhood
+                for i in range(ship.y, ship.y+ship.length):
+                    if (self.check_for_ship(ship.x-1, i-1) or self.check_for_ship(ship.x, i-1)
+								or self.check_for_ship(ship.x+1, i-1) or self.check_for_ship(ship.x-1, i)
+								or self.check_for_ship(ship.x, i) or self.check_for_ship(ship.x+1, i)
+                                or self.check_for_ship(ship.x-1, i+1) or self.check_for_ship(ship.x, i+1)
+                                or self.check_for_ship(ship.x+1, i+1)):
+                                    return False
+        #down
+        elif ship.direction == 2:
+            if ship.length > (self.rows - ship.x):
+                return False
+            else:
+                #Check for ships in the neighborhood
+                for i in range(ship.x, ship.length+ship.x):
+                    if (self.check_for_ship(i+1, ship.y-1) or self.check_for_ship(i+1, ship.y)
+								or self.check_for_ship(i+1, ship.y+1) or self.check_for_ship(i, ship.y-1)
+								or self.check_for_ship(i, ship.y) or self.check_for_ship(i, ship.y+1)
+                                or self.check_for_ship(i-1, ship.y-1) or self.check_for_ship(i-1, ship.y)
+                                or self.check_for_ship(i-1, ship.y+1)):
+                                    return False
+        #left
+        elif ship.direction == 3:
+            if ship.length > (ship.y+1):
+                return False
+            else:
+                #Check for ships in the neighborhood
+                for i in range(ship.y, ship.y-ship.length, -1):
+                    if (self.check_for_ship(ship.x-1, i-1) or self.check_for_ship(ship.x, i-1)
+								or self.check_for_ship(ship.x+1, i-1) or self.check_for_ship(ship.x-1, i)
+								or self.check_for_ship(ship.x, i) or self.check_for_ship(ship.x+1, i)
+                                or self.check_for_ship(ship.x-1, i+1) or self.check_for_ship(ship.x, i+1)
+                                or self.check_for_ship(ship.x+1, i+1)):
+                                    return False
+        return True
 
 battlefield = Grid(grid_x, grid_y)
 
@@ -121,18 +107,18 @@ ship_length = 2
 for _ in range(number_of_ships):
     ship = Ship(random.randrange(grid_x), random.randrange(grid_y),
                 random.randrange(4), ship_length)
-                #0, ship_length)
     ships.append(ship)
     ship_length += 1
 for ship in ships:
-    valid_ship = ship.is_valid(battlefield)
+    valid_ship = battlefield.is_ship_valid(ship)
     if valid_ship:
         battlefield.place_ship(ship)
     else:
         while valid_ship == False:
             ship.generate_random_ship(grid_x, grid_y, ship.length)
-            valid_ship = ship.is_valid(battlefield)
+            valid_ship = battlefield.is_ship_valid(ship)
         battlefield.place_ship(ship)
 print(battlefield.grid)
+print()
 for ship in ships:
     ship.print_ship()
