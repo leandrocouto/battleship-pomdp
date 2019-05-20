047f2487fbee65c7761298442f938f74baec9653
Leandro Couto Medeiros <leandrocoutom@gmail.com>
2019-05-20 17:14:10 -0300

    ADD partial implementation of tree expansion phase

--- node.py
+++ node.py
@@ -1,11 +1,10 @@
 import copy
 class Node:
-	def __init__(self, isRoot, children, parent_key, n_visits, value, particle_list):
-		self.isRoot = isRoot
+	def __init__(self, parent, children, n_visits, value, particle_list):
+		self.parent = parent
         #Dictionary where key is the index of the action/observation (node)
         #Value is the key of the child node
 		self.children = copy.copy(children)
-		self.parent_key = parent_key
 		self.n_visits = n_visits
 		self.value = value
 		self.particle_list = copy.copy(particle_list)
--- tree.py
+++ tree.py
@@ -6,8 +6,20 @@ class Tree:
         #Key is the 'count' (it will be unique because the count can only grow)
         #Value is the node itself
 		self.nodes[self.count] = root_node
-	def expand(self):
-		print()
+	def expand(self, parent, index, IsActionNode = False):
+        self.count += 1
+        if IsActionNode: 
+        	node = Node(parent, {}, 0, 0, -1)
+            # add node to tree
+            self.nodes[self.count] = node 
+            # inform parent node
+            self.nodes[parent][1][index] = self.count 
+        else:
+            node = Node(parent, {}, 0, 0, [])
+            # add node to tree
+            self.nodes[self.count] = node
+            # inform parent node
+            self.nodes[parent][1][index] = self.count
 	def prune_from_node(self, node):
 		print()
 	def update_root(self, node):
