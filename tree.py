e8ffbbcbd7c332c085bc416a47c843b381fb2420
Leandro Couto Medeiros <leandrocoutom@gmail.com>
2019-05-23 17:03:13 -0300

    ADD prune tree method

--- tree.py
+++ tree.py
@@ -20,9 +20,29 @@ class Tree:
             self.nodes[self.count] = node
             # inform parent node
             self.nodes[parent].children[index] = self.count
-	def prune_from_node(self, node):
-		print()
-	def update_root(self, node):
+    def get_node_after_observation(self, node_after_action, observation):
+    	# Check if a given observation node has been visited
+        if observation not in list(self.nodes[node_after_action].children.keys()):
+            # If not create the node
+            self.expand(node_after_action, observation)
+        # Get the nodes index
+        return self.nodes[node_after_action].children[observation]
+	def prune(self, action, observation):
+		# Get the node after action
+        node_after_action = self.nodes[-1].children[action]
+
+        # Get the node after the observation (which will be the new root)
+        new_root = self.get_node_after_observation(node_after_action, observation)
+
+        # remove new_root from parent's children to avoid deletion
+        del self.nodes[node_after_action].children[observation]
+
+        # delete all new_root's "brothers"
+        self.prune(-1)
+
+        # set new_root as root (key = -1)
+        self.update_root(new_root)
+	def update_root(self, new_root):
 		print()
 	def is_leaf_node(self, node):
 		if len(node.children) == 0:
