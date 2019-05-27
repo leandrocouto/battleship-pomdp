1fbfcad1c043af9275562a077cdf1a6210b6b6dd
Leandro Couto Medeiros <leandrocoutom@gmail.com>
2019-05-27 16:41:42 -0300

    ADD Search routine to POMCP algorithm

--- main.py
+++ main.py
@@ -10,34 +10,34 @@ tree.expand(-1, 500)
 tree.expand(-1, 501)
 tree.expand(-1, 502)
 #one of the children has two children
-tree.expand(501, 503)
-tree.expand(501, 504)
+tree.expand(1, 503)
+tree.expand(1, 504)
 #
 #
-#     |------ 500
-#     |               |---- 503
-#    -1------ 501 -------
-#     |               |---- 504
-#     |------ 502
+#     |------ 500(0)
+#     |                      |---- 503(3)
+#    -1------ 501(1)  -------
+#     |                      |---- 504(4)
+#     |------ 502(2)
 #
-for i in range(500, 505):
+for i in range(-1, 5):
     if tree.is_leaf_node(tree.nodes[i]):
         print(i, 'is leaf node')
     else:
         print(i, 'is not leaf node')
 
 print('\nBefore pruning- Size of nodes{} = ', len(tree.nodes), '\n') 
-tree.prune(504)
+tree.prune(4)
 print('\nAfter pruning 504 - Size of nodes{} = ', len(tree.nodes), '\n')
-for i in range(500, 504):
+for i in range(-1, 4):
     if tree.is_leaf_node(tree.nodes[i]):
         print(i, 'is leaf node')
     else:
         print(i, 'is not leaf node')
 
-tree.prune(503)
+tree.prune(3)
 print('\nAfter pruning 503 - Size of nodes{} = ', len(tree.nodes), '\n')
-for i in range(500, 503):
+for i in range(-1, 3):
     if tree.is_leaf_node(tree.nodes[i]):
         print(i, 'is leaf node')
     else:
--- pomcp.py
+++ pomcp.py
@@ -9,8 +9,42 @@ class POMCP:
         self.timeout = timeout
         self.n_particles = n_particles
         self.tree = Tree()
-    def simulate():
+    def simulate(self):
         print()
-    def rollout():
+    def rollout(self):
         print()
+    def search(self):
+        particles = self.tree.nodes[-1].particle_list.copy()
+        #Loop until timeout
+        for _ in range(self.timeout):
+            if len(particles) == 0:
+                state = choice(self.states)
+            else:
+                state = choice(particles)
+            self.simulate(state, -1, 0)
+        best_action, _ = self.SearchBest(-1, UseUCB = False)
+        return best_action
+    def search_best(self, h):
+        max_value = None
+        result = None
+        resulta = None
+        #if it is not an action node
+        if self.tree.nodes[h].particle_list != -1:
+            children = self.tree.nodes[h].children
+            # UCB for each child node
+            for action, child in children.items():
+                # if node is unvisited return it
+                if self.tree.nodes[child].n_visits == 0:
+                    return action, child
+                ucb = UCB(self.tree.nodes[h].n_visits, self.tree.nodes[child].n_visits, 
+                self.tree.nodes[child].value, self.c)
+        
+                # Max is kept 
+                if max_value is None or max_value < ucb:
+                    max_value = ucb
+                    result = child
+                    resulta = action
+        #return action-child_id values
+        return resulta, result
+
     
